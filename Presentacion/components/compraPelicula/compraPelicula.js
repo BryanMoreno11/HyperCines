4d39d9e47c33dd321f9fcb2390c63a046d34a453
BryanMoreno11 <bmoreno3@utmachala.edu.ec>
2024-02-18 13:31:03 -0500

    validacion seleccion de asientos

--- Presentacion/components/compraPelicula/compraPelicula.js
+++ Presentacion/components/compraPelicula/compraPelicula.js
@@ -1,10 +1,17 @@
+//--------------------------------------Variables Globales----------------------------
 let id_funcion = 1;
+let cant = 1;
 var app = angular.module("compra", []);
+let asientos_seleccionados = [];
+
+//-------------------------------------------Lógica del componente------------------
 app.controller("compraController", function($scope) {
     //variables
     $scope.asientos;
     $scope.capacidad;
     $scope.asientos_ocupados;
+    $scope.seat_aux;
+    $scope.posicion_aux;
     //Llamadas
     getCapacidad().then(function(response) {
         $scope.capacidad = response[0].capacidad_sala;
@@ -43,6 +50,30 @@ app.controller("compraController", function($scope) {
         }
         $scope.asientos = asientos_sala;
     }
+
+    $scope.selectSeat = function(e, posicion) {
+        let selectedSeats = document.querySelectorAll('.fila .seat.selected');
+        if (selectedSeats.length == cant && !(e.target.classList.contains("selected"))) {
+            $scope.seat_aux.classList.toggle('selected');
+            asientos_seleccionados.splice(asientos_seleccionados.indexOf($scope.posicion_aux), 1);
+        }
+        if (!e.target.classList.contains('occupied')) {
+            e.target.classList.toggle('selected');
+        }
+        selectedSeats = document.querySelectorAll('.fila .seat.selected');
+        if (selectedSeats.length == cant) {
+            $scope.seat_aux = e.target;
+            $scope.posicion_aux = posicion;
+        }
+        console.log(e.target);
+        if (e.target.classList.contains("selected")) {
+            asientos_seleccionados.push(posicion);
+        } else {
+            asientos_seleccionados.splice(asientos_seleccionados.indexOf(posicion), 1);
+        }
+        console.log(asientos_seleccionados);
+        updateSelectedCount();
+    }
 });
 //--------------------------------------Manipulación del DOM--------------------------------------
 const container = document.querySelector('.container');
@@ -58,8 +89,7 @@ const cantAsientos = document.querySelector(".cantidad");
 const frmPayMethods = document.querySelector(".pay-methods");
 let precioEntrada = document.getElementById('priceEntry').textContent;
 let ticketPrice = document.querySelector(".totallyEntry").textContent;
-let cant = 0;
-let capacidad;
+
 //Métodos------------------------------------------------------------------
 btnSiguiente.addEventListener("click", e => {
     window.alert("Ahora estas en   las entradas");
@@ -71,10 +101,17 @@ btnSiguiente.addEventListener("click", e => {
 });
 
 btnVolver.addEventListener("click", e => {
+    window.alert("volvio a la pantalla princial");
     frmDetails.classList.remove("hide");
     frmDetails.classList.add("show");
     frmSeats.classList.remove("show");
     frmSeats.classList.add("hide");
+    let selectedSeats = document.querySelectorAll('.fila .seat.selected');
+    selectedSeats.forEach(function(seat) {
+        seat.classList.remove("selected");
+    });
+    updateSelectedCount();
+    asientos_seleccionados = [];
 });
 
 btnNextSeat.addEventListener("click", e => {
@@ -85,20 +122,27 @@ btnNextSeat.addEventListener("click", e => {
 });
 
 btnVolverPay.addEventListener("click", e => {
-    window.alert("Ahora estas volviando a  las entradas");
-    frmPayMethods.classList.remove("show");
-    frmPayMethods.classList.add("hide");
-    frmSeats.classList.remove("hide");
-    frmSeats.classList.add("show");
-})
+        window.alert("Ahora estas volviando a  las entradas");
+        frmPayMethods.classList.remove("show");
+        frmPayMethods.classList.add("hide");
+        frmSeats.classList.remove("hide");
+        frmSeats.classList.add("show");
+    })
+    /*
+    container.addEventListener('click', (e) => {
+        let selectedSeats = document.querySelectorAll('.fila .seat.selected');
+        if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+            if (selectedSeats.length == cant && !(e.target.classList.contains("selected"))) {
+                selectedSeats[cant - 1].classList.toggle('selected');
+            }
+            e.target.classList.toggle('selected');
+        }
+        selectedSeats = document.querySelectorAll('.fila .seat.selected');
+        console.log(selectedSeats);
+        updateSelectedCount();
+    });
+    */
 
-container.addEventListener('click', (e) => {
-    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        e.target.classList.toggle('selected');
-    }
-
-    updateSelectedCount();
-});
 
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.fila .seat.selected');
@@ -114,7 +158,7 @@ btnPlus.addEventListener("click", e => {
 });
 
 btnMinus.addEventListener("click", e => {
-    if (cant !== 0) {
+    if (cant > 1) {
         cant = cant - 1;
         cantAsientos.innerHTML = cant;
     }
