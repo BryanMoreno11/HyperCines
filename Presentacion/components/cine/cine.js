3be2d210a3360cc22facfe780a04aeace0b9896a
Adrian <avega8640@gmail.com>
2024-02-24 15:00:17 -0500

    adrian

--- Presentacion/components/cine/cine.js
+++ Presentacion/components/cine/cine.js
@@ -76,3 +76,46 @@ async function cargarPeliculasFuncion(ciudad, complejo) {
     return data;
 
 }
+
+const btnLeft = document.querySelector(".btn-left");
+const btnRight = document.querySelector(".btn-right");
+const slider = document.querySelector("#slider");
+const sliderSections = document.querySelectorAll(".slider-section");
+const numImages = sliderSections.length; // Número total de imágenes
+let counter = 0; // Iniciar desde 0 para mostrar la primera imagen al principio
+const widthImg = 100 / numImages;
+let intervalID;
+
+btnLeft.addEventListener("click", e => {
+    clearInterval(intervalID);
+    moveToLeft();
+});
+
+btnRight.addEventListener("click", e => {
+    clearInterval(intervalID);
+    moveToRight();
+});
+
+intervalID = setInterval(() => {
+    moveToRight();
+}, 10000); // 10 segundos
+
+function moveToRight() {
+    counter++;
+    if (counter >= numImages) {
+        counter = 0; // Vuelve a la primera imagen después de la última
+    }
+    const operacion = widthImg * counter;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "transform ease .6s"; // Solo transición de transformación
+}
+
+function moveToLeft() {
+    counter--;
+    if (counter < 0) {
+        counter = numImages - 1; // Va a la última imagen después de la primera
+    }
+    const operacion = widthImg * counter;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "transform ease .6s"; // Solo transición de transformación
+}
